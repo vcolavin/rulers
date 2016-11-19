b30c4c462fa24097f25b7d2bbec88bda9009fe0d
Vincent Colavin <vcolavin@focusvision.com>
2016-11-19 15:48:44 -0800

    add hello world code

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,5 +1,13 @@
-require "rulers/version"
+require 'rulers/version'
 
 module Rulers
-  # Your code goes here...
+  class Application
+    def call(env)
+      [
+        200,
+        { 'Content-Type' => 'text/html' },
+        ['hello from rulers!']
+      ]
+    end
+  end
 end
