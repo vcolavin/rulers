2f1cf0b6e91c78b364783f0d4206c334234c10fd
Vincent Colavin <vcolavin@focusvision.com>
2016-11-19 22:11:45 -0800

    begin writing controller code

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,14 +1,26 @@
 require 'rulers/version'
+require 'rulers/routing'
 
 module Rulers
   class Application
     `echo debug > debug.txt`
     def call(env)
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
       [
         200,
         { 'Content-Type' => 'text/html' },
         ['hello from rulers!']
       ]
+    end
+  end
+
+  class Controller
+    attr_reader :env
+
+    def initialize(env)
+      @env = env
     end
   end
 end
