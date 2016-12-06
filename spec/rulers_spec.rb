bce2c9fb2b5e89146c843720b616983c7e39a15b
vcolavin <vcolavin@gmail.com>
2016-12-05 22:04:58 -0800

    update failing test

--- spec/rulers_spec.rb
+++ spec/rulers_spec.rb
@@ -1,6 +1,18 @@
 require 'spec_helper'
 include Rack::Test::Methods
 
+class TestController < Rulers::Controller
+  def index
+    'Hello!'
+  end
+end
+
+class TestApp < Rulers::Application
+  def get_controller_and_action(env)
+    [TestController, 'index']
+  end
+end
+
 describe Rulers do
   it 'has a version number' do
     expect(Rulers::VERSION).not_to be nil
